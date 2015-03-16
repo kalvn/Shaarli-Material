65ed02a85408cad899114d6a924d08dbd257ef62
kalvn <clement.koenig@gmail.com>
2015-03-16 10:26:14 +0100

    First commit.

---
+++ material/inc/main.js
@@ -0,0 +1,118 @@
+function confirmDeleteLink() { 
+    return confirm("Are you sure you want to delete this link ?");
+}
+
+$(document).ready(function(){
+    $('html').on('click', function(event){
+        if($.inArray('popup-trigger', event.target.classList) > -1 || $(event.target).parents('.popup-trigger').length >= 1){
+            // Nothing to do.
+        } else{
+            $('.popup').hide();
+        }
+    });
+
+    $('.popup-trigger').on('click', function(){
+
+        // var $popup = $(this).find('.popup');
+        var $popup = $('#' + $(this).data('popup'));
+
+        if($popup.is(':visible')){
+            $popup.fadeOut(400);
+        } else{
+            $('.popup').hide();
+            $popup.fadeIn(400, function(){
+                 $(this).find('input[type=text]').first().focus();
+            });
+        }
+    });
+
+    $('.popup').on('click', function(event){
+        if(event.target.tagName !== 'A'){
+            event.preventDefault();
+            return false;
+        }
+    });
+
+    // Search field for tags.
+    $('#toolbar-button-filter').on('click', function(){
+        var val = $('#searchform_value').val();
+        $('#tagfilter_value').val(val);
+        $('#hidden-tag-form').submit();
+
+        return false;
+    });
+
+    // Change date format for recent entries.
+    $('.link-actual-date').each(function(index){
+        var newDate = moment($(this).html(), 'DD/MM/YYYY HH:mm:ss').fromNow();
+        $(this).html(newDate);
+    });
+
+    // Hide empty thumbnails.
+    $('.thumb').each(function(){
+        if($(this).html().trim() === ''){
+            $(this).remove();
+        }
+    });
+
+    // QR Code.
+    $('.icon-qrcode').on('click', function(){
+        var url = $(this).attr('href');
+        var $imagePopup = $('#image-popup').first();
+        var $overlay = $('#overlay').first();
+        if(url && $imagePopup && $overlay){
+            $imagePopup.html('<img src="' + url + '" alt="QR Code" />').fadeIn();
+            $overlay.fadeIn();
+        }
+
+        // Disable original click event.
+        return false;
+    });
+    $('#overlay').on('click', function(){
+        $('#image-popup').fadeOut();
+        $(this).fadeOut();
+    });
+
+    // Material forms.
+    $('input[type=text],input[type=search],input[type=password],textarea').on('focus', function(){
+        var $input = $(this);
+        var id = $input.attr('id');
+
+        if(typeof id === 'string' && id !== ''){
+            var $label = $('label[for=' + id + ']');
+            if($label.length > 0){
+                $label.addClass('active');
+            }
+        }
+    }).on('blur', function(){
+        var $input = $(this);
+        var id = $input.attr('id');
+
+        if(typeof id === 'string' && id !== ''){
+            var $label = $('label[for=' + id + ']');
+            if($label.length > 0){
+                $label.removeClass('active');
+            }
+        }
+    });
+
+    $('input.tags-autocomplete').autocomplete({
+        lookup: function (query, done) {
+            var results = [];
+
+            $.getJSON(shaarli.source + '?ws=singletag&term=' + query, function(data){
+                var obj = JSON.parse(data);
+                $.each(obj, function(i, res){
+                    results.push({
+                        value: res,
+                        data: 'pouic'
+                    });
+                });
+            });
+
+            done(results);
+        }
+        /*paramName: 'term',
+        serviceUrl: shaarli.source + '?ws=singletag'*/
+    });
+});
