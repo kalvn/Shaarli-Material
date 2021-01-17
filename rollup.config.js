04ef294feb26c89c351ffdc2e9ec249a249896e2
kalvn <kalvn@pm.me>
2021-01-17 23:32:02 +0100

    Minifies JS when building for prod.

--- rollup.config.js
+++ rollup.config.js
@@ -3,13 +3,13 @@ import path from 'path';
 import resolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
-import { terser } from "rollup-plugin-terser"; // minify JS
 import postcss from 'rollup-plugin-postcss';
 import clean from 'postcss-clean';
 import autoprefixer from 'autoprefixer';
 import copy from 'rollup-plugin-copy';
 // import purgecss from '@fullhuman/postcss-purgecss';
 import eslint from '@rollup/plugin-eslint';
+import { terser } from "rollup-plugin-terser"; // minify JS
 
 const config = {
   input: 'src/js/main.js',
@@ -26,7 +26,7 @@ const config = {
         'src/scss/**'
       ]
     }),
-    // terser(),
+    process.env.NODE_ENV === 'production' ? terser() : null,
     postcss({
       extract: path.resolve('material/dist/bundle.css'),
       plugins: [
